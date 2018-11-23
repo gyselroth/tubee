e92a43ababb114f16d967396fa8ca84b7634e32e
raffis <sahli@gyselroth.com>
2018-11-23 23:28:45 +0100

    i

---
+++ src/lib/Endpoint/Json/Validator.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * tubee.io
+ *
+ * @copyright   Copryright (c) 2017-2018 gyselroth GmbH (https://gyselroth.com)
+ * @license     GPL-3.0 https://opensource.org/licenses/GPL-3.0
+ */
+
+namespace Tubee\Endpoint\Json;
+
+use InvalidArgumentException;
+use Tubee\Storage\Validator as StorageValidator;
+
+class Validator
+{
+    /**
+     * Validate resource.
+     */
+    public static function validate(array $resource): array
+    {
+        $defaults = [
+            'storage' => [
+                'type' => 'Stream',
+            ],
+        ];
+
+        if (!isset($resource['file']) || !is_string($resource['file'])) {
+            throw new InvalidArgumentException('file is required and must be a string');
+        }
+
+        $resource = array_replace_recursive($defaults, $resource);
+        $resource['storage'] = StorageValidator::validate($resource['storage']);
+
+        return $resource;
+    }
+}
