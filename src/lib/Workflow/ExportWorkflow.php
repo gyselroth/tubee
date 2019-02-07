6cedceec874037b32909378aac871cc073eb15de
raffis <sahli@gyselroth.com>
2019-02-07 15:36:15 +0100

    added workflows

---
+++ src/lib/Workflow/ExportWorkflow.php
@@ -0,0 +1,213 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * tubee
+ *
+ * @copyright   Copryright (c) 2017-2019 gyselroth GmbH (https://gyselroth.com)
+ * @license     GPL-3.0 https://opensource.org/licenses/GPL-3.0
+ */
+
+namespace Tubee\Workflow;
+
+use Generator;
+use MongoDB\BSON\UTCDateTimeInterface;
+use Tubee\DataObject\DataObjectInterface;
+use Tubee\Endpoint\Exception as EndpointException;
+use Tubee\EndpointObject\EndpointObjectInterface;
+use Tubee\Helper;
+use Tubee\Workflow;
+
+class ExportWorkflow extends Workflow
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function export(DataObjectInterface $object, UTCDateTimeInterface $ts, bool $simulate = false): bool
+    {
+        $attributes = $object->toArray();
+        $attributes['relations'] = iterator_to_array($this->getRelations($object));
+        if ($this->checkCondition($attributes) === false) {
+            return false;
+        }
+
+        $map = $this->attribute_map->map($attributes, $ts);
+        $this->logger->info('mapped object attributes [{map}] for write', [
+            'category' => get_class($this),
+            'map' => array_keys($map),
+        ]);
+
+        $exists = $this->getExportObject([
+            'map' => $map,
+            'object' => $attributes,
+        ]);
+
+        $map = Helper::pathArrayToAssociative($map);
+        $ensure = $this->ensure;
+
+        if ($exists === null && $this->ensure === WorkflowInterface::ENSURE_ABSENT) {
+            $this->logger->info('skip object which is already absent from endpoint ['.$this->endpoint->getIdentifier().']', [
+                'category' => get_class($this),
+            ]);
+
+            return true;
+        }
+        if ($exists !== null && $this->ensure === WorkflowInterface::ENSURE_EXISTS) {
+            return false;
+        }
+
+        if ($exists === null && $this->ensure === WorkflowInterface::ENSURE_LAST) {
+            $ensure = WorkflowInterface::ENSURE_EXISTS;
+        }
+
+        switch ($ensure) {
+            case WorkflowInterface::ENSURE_ABSENT:
+                return $this->ensureAbsent($exists, $map, $simulate);
+            case WorkflowInterface::ENSURE_EXISTS:
+                return $this->ensureExists($object, $map, $ts, $simulate);
+            default:
+            case WorkflowInterface::ENSURE_LAST:
+                return $this->ensureLast($object, $exists, $map, $ts, $simulate);
+        }
+
+        return false;
+    }
+
+    /**
+     * Update object on endpoint.
+     */
+    protected function ensureLast(DataObjectInterface $object, EndpointObjectInterface $exists, array $map, UTCDateTimeInterface $ts, bool $simulate = false): bool
+    {
+        $this->logger->info('change object on endpoint ['.$this->endpoint->getIdentifier().']', [
+            'category' => get_class($this),
+        ]);
+
+        $diff = $this->attribute_map->getDiff($map, $exists->getData());
+
+        $endpoints = [$this->endpoint->getName() => [
+            'last_sync' => $ts,
+            'garbage' => false,
+        ]];
+
+        if (count($diff) > 0) {
+            $this->logger->info('update object on endpoint ['.$this->endpoint->getIdentifier().'] with attributes [{attributes}]', [
+                'category' => get_class($this),
+                'attributes' => $diff,
+            ]);
+
+            $diff = $this->endpoint->getDiff($this->attribute_map, $diff);
+
+            $this->logger->debug('execute diff [{diff}] on endpoint ['.$this->endpoint->getIdentifier().']', [
+                'category' => get_class($this),
+                'diff' => $diff,
+            ]);
+
+            $result = $this->endpoint->change($this->attribute_map, $diff, $map, $exists->getData(), $simulate);
+
+            if ($result !== null) {
+                $endpoints[$this->endpoint->getName()]['result'] = $result;
+            }
+        } else {
+            $this->logger->debug('object on endpoint ['.$this->endpoint->getIdentifier().'] is already up2date', [
+                'category' => get_class($this),
+            ]);
+        }
+
+        if (!isset($endpoints[$this->endpoint->getName()]['result'])) {
+            if (isset($exists->getData()[$this->endpoint->getResourceIdentifier()])) {
+                $endpoints[$this->endpoint->getName()]['result'] = $exists->getData()[$this->endpoint->getResourceIdentifier()];
+            } else {
+                $endpoints[$this->endpoint->getName()]['result'] = null;
+            }
+        }
+
+        $this->endpoint->getCollection()->changeObject($object, $object->toArray(), $simulate, $endpoints);
+
+        return true;
+    }
+
+    /**
+     * Create object on endpoint.
+     */
+    protected function ensureExists(DataObjectInterface $object, array $map, UTCDateTimeInterface $ts, bool $simulate = false)
+    {
+        $this->logger->info('create new object on endpoint ['.$this->endpoint->getIdentifier().']', [
+            'category' => get_class($this),
+        ]);
+
+        $result = $this->endpoint->create($this->attribute_map, $map, $simulate);
+
+        $endpoints = [
+            $this->endpoint->getName() => [
+                'last_sync' => $ts,
+                'result' => $result,
+                'garbage' => false,
+            ],
+        ];
+
+        $this->endpoint->getCollection()->changeObject($object, $object->toArray(), $simulate, $endpoints);
+
+        return true;
+    }
+
+    /**
+     * Remove object from endpoint.
+     */
+    protected function ensureAbsent(EndpointObjectInterface $exists, array $map, bool $simulate = false)
+    {
+        $this->logger->info('delete existing object from endpoint ['.$this->endpoint->getIdentifier().']', [
+            'category' => get_class($this),
+        ]);
+
+        $this->endpoint->delete($this->attribute_map, $map, $exists->getData(), $simulate);
+
+        return true;
+    }
+
+    /**
+     * Transform relations to array.
+     */
+    protected function getRelations(DataObjectInterface $object): Generator
+    {
+        foreach ($object->getRelations() as $relation) {
+            $resource = $relation->toArray();
+            $resource['object'] = $relation->getDataObject()->toArray();
+            yield $resource;
+        }
+    }
+
+    /**
+     * Get export object.
+     */
+    protected function getExportObject(array $map): ?EndpointObjectInterface
+    {
+        try {
+            if ($this->endpoint->flushRequired()) {
+                $exists = null;
+            } else {
+                $exists = $this->endpoint->getOne($map, $this->attribute_map->getAttributes());
+            }
+
+            $this->logger->debug('found existing object on destination endpoint with provided filter_one', [
+                'category' => get_class($this),
+            ]);
+
+            return $exists;
+        } catch (EndpointException\ObjectMultipleFound $e) {
+            throw $e;
+        } catch (EndpointException\ObjectNotFound $e) {
+            $this->logger->debug('object does not exists yet on destination endpoint', [
+                'category' => get_class($this),
+                'exception' => $e,
+            ]);
+        } catch (EndpointException\AttributeNotResolvable $e) {
+            $this->logger->debug('object filter can not be resolved, leading to non existing object', [
+                'category' => get_class($this),
+                'exception' => $e,
+            ]);
+        }
+
+        return null;
+    }
+}
---
+++ src/lib/Workflow/ImportWorkflow.php
@@ -0,0 +1,224 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * tubee
+ *
+ * @copyright   Copryright (c) 2017-2019 gyselroth GmbH (https://gyselroth.com)
+ * @license     GPL-3.0 https://opensource.org/licenses/GPL-3.0
+ */
+
+namespace Tubee\Workflow;
+
+use MongoDB\BSON\UTCDateTimeInterface;
+use Tubee\Collection\CollectionInterface;
+use Tubee\DataObject\DataObjectInterface;
+use Tubee\DataObject\Exception as DataObjectException;
+use Tubee\EndpointObject\EndpointObjectInterface;
+use Tubee\Helper;
+use Tubee\Workflow;
+
+class ImportWorkflow extends Workflow
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function cleanup(DataObjectInterface $object, UTCDateTimeInterface $ts, bool $simulate = false): bool
+    {
+        $attributes = $object->toArray();
+        if ($this->checkCondition($attributes) === false) {
+            return false;
+        }
+
+        $attributes = Helper::associativeArrayToPath($attributes);
+        $map = $this->attribute_map->map($attributes, $ts);
+        $this->logger->info('mapped object attributes [{map}] for cleanup', [
+            'category' => get_class($this),
+            'map' => array_keys($map),
+        ]);
+
+        switch ($this->ensure) {
+            case WorkflowInterface::ENSURE_ABSENT:
+                return $this->endpoint->getCollection()->deleteObject($object->getId(), $simulate);
+
+            break;
+            default:
+            case WorkflowInterface::ENSURE_LAST:
+                $resource = Map::map($this->attribute_map, $map, ['data' => $object->getData()], $ts);
+                $object->getCollection()->changeObject($object, $resource, $simulate);
+                $this->importRelations($object, $map, $simulate);
+
+                return true;
+
+            break;
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function import(CollectionInterface $collection, EndpointObjectInterface $object, UTCDateTimeInterface $ts, bool $simulate = false): bool
+    {
+        $object = $object->getData();
+        if ($this->checkCondition($object) === false) {
+            return false;
+        }
+
+        $map = $this->attribute_map->map($object, $ts);
+        $this->logger->info('mapped object attributes [{map}] for import', [
+            'category' => get_class($this),
+            'map' => array_keys($map),
+        ]);
+
+        $exists = $this->getImportObject($collection, $map, $object, $ts);
+
+        if ($exists === null) {
+            $this->logger->info('found no existing data object for given import attributes', [
+                'category' => get_class($this),
+            ]);
+        } else {
+            $this->logger->info('identified existing data object ['.$exists->getId().'] for import', [
+                'category' => get_class($this),
+            ]);
+        }
+
+        $ensure = $this->ensure;
+        if ($exists !== null && $this->ensure === WorkflowInterface::ENSURE_EXISTS) {
+            return false;
+        }
+        if ($exists === null && $this->ensure === WorkflowInterface::ENSURE_LAST) {
+            $ensure = WorkflowInterface::ENSURE_EXISTS;
+        }
+
+        switch ($ensure) {
+            case WorkflowInterface::ENSURE_ABSENT:
+                $collection->deleteObject($exists->getId(), $simulate);
+
+                return true;
+
+            break;
+            case WorkflowInterface::ENSURE_EXISTS:
+                $endpoints = [
+                    $this->endpoint->getName() => [
+                        'last_sync' => $ts,
+                        'garbage' => false,
+                    ],
+                ];
+
+                $id = $collection->createObject(Helper::pathArrayToAssociative($map), $simulate, $endpoints);
+                $this->importRelations($collection->getObject(['_id' => $id]), $map, $simulate, $endpoints);
+
+                return true;
+
+            break;
+            default:
+            case WorkflowInterface::ENSURE_LAST:
+                $object = Map::map($this->attribute_map, $map, ['data' => $exists->getData()], $ts);
+                $endpoints = [
+                    $this->endpoint->getName() => [
+                        'last_sync' => $ts,
+                        'garbage' => false,
+                    ],
+                ];
+
+                $collection->changeObject($exists, $object, $simulate, $endpoints);
+                $this->importRelations($exists, $map, $simulate, $endpoints);
+
+                return true;
+
+            break;
+        }
+
+        return false;
+    }
+
+    /**
+     * Create object relations.
+     */
+    protected function importRelations(DataObjectInterface $object, array $data, bool $simulate, array $endpoints = []): bool
+    {
+        $this->logger->debug('find relationships to be imported for object ['.$object->getId().']', [
+            'category' => get_class($this),
+        ]);
+
+        foreach ($this->attribute_map->getMap() as $definition) {
+            if (!isset($definition['map'])) {
+                continue;
+            }
+
+            if (!isset($data[$definition['name']])) {
+                $this->logger->debug('relation attribute ['.$definition['map']['collection'].':'.$definition['map']['to'].'] not found in mapped data object', [
+                    'category' => get_class($this),
+                ]);
+
+                continue;
+            }
+
+            $this->logger->debug('find related object from ['.$object->getId().'] to ['.$definition['map']['collection'].':'.$definition['map']['to'].'] => ['.$data[$definition['name']].']', [
+                'category' => get_class($this),
+            ]);
+
+            $namespace = $this->endpoint->getCollection()->getResourceNamespace();
+            $collection = $namespace->getCollection($definition['map']['collection']);
+            $relative = $collection->getObject([
+                $definition['map']['to'] => $data[$definition['name']],
+            ]);
+
+            $this->logger->debug('ensure relation state ['.$definition['map']['ensure'].'] for relation to ['.$relative->getId().']', [
+                'category' => get_class($this),
+            ]);
+
+            switch ($definition['map']['ensure']) {
+                case WorkflowInterface::ENSURE_EXISTS:
+                case WorkflowInterface::ENSURE_LAST:
+                    $namespace = $this->endpoint->getCollection()->getResourceNamespace()->getName();
+                    $collection = $this->endpoint->getCollection()->getName();
+                    $ep = $this->endpoint->getName();
+
+                    $endpoints = [
+                        join('/', [$namespace, $collection, $ep]) => $endpoints[$ep],
+                    ];
+
+                    $object->createOrUpdateRelation($relative, [], $simulate, $endpoints);
+
+                break;
+                default:
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Get import object.
+     */
+    protected function getImportObject(CollectionInterface $collection, array $map, array $object, UTCDateTimeInterface $ts): ?DataObjectInterface
+    {
+        $filter = array_intersect_key($map, array_flip($this->endpoint->getImport()));
+        //TODO: debug import line here
+
+        if (empty($filter) || count($filter) !== count($this->endpoint->getImport())) {
+            throw new Exception\ImportConditionNotMet('import condition attributes are not available from mapping');
+        }
+
+        try {
+            $exists = $collection->getObject($filter, false);
+        } catch (DataObjectException\MultipleFound $e) {
+            throw $e;
+        } catch (DataObjectException\NotFound $e) {
+            return null;
+        }
+
+        /*$endpoints = $exists->getEndpoints();
+
+        if ($exists !== false && isset($endpoints[$this->endpoint->getName()])
+        && $endpoints[$this->endpoint->getName()]['last_sync']->toDateTime() >= $ts->toDateTime()) {
+            throw new Exception\ImportConditionNotMet('import filter matched multiple source objects');
+        }*/
+
+        return $exists;
+    }
+}
