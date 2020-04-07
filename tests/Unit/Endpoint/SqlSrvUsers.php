d699a52ddccd7695cd1bad5c5fd560311017d2f3
s-aebischer <aebischer@gyselroth.com>
2020-04-07 10:56:23 +0200

    initial set of SqlSrvUsers tests

--- tests/Unit/Endpoint/SqlSrvUsers.php
+++ tests/Unit/Endpoint/SqlSrvUsers.php
@@ -13,78 +13,293 @@ namespace Tubee\Testsuite\Unit\Endpoint;
 
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
+use Tubee\AttributeMap\AttributeMapInterface;
 use Tubee\Collection\CollectionInterface;
 use Tubee\Endpoint\EndpointInterface;
+use Tubee\Endpoint\Exception\AttributeNotResolvable as AttributeNotResolvable;
 use Tubee\Endpoint\SqlSrvUsers;
+use Tubee\Endpoint\SqlSrvUsers\Exception\InvalidQuery as InvalidQuery;
 use Tubee\Endpoint\SqlSrvUsers\Wrapper;
 use Tubee\Endpoint\SqlSrvUsers\Wrapper as SqlSrvWrapper;
 use Tubee\Workflow\Factory as WorkflowFactory;
 
 class SqlSrvUsersTest extends TestCase
 {
-//    public function testSetupDefaultSettings()
-//    {
-//        $wrapper = $this->createMock(SqlSrvWrapper::class);
-//        $sqlSrvUsers = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, 'foobar', $wrapper, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class));
-//        $sqlSrvUsers->setup();
-//    }
-//
-//    public function testShutdown()
-//    {
-//        $wrapper = $this->createMock(SqlSrvWrapper::class);
-//        $sqlSrvUsers = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, 'foobar', $wrapper, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class));
-//        $sqlSrvUsers->shutdown();
-//    }
-//
-//    public function testTransformAndQuery()
-//    {
-//        $sqlSrvUsers = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, 'foobar', $this->createMock(Wrapper::class), $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class));
-//
-//        $query = [
-//            '$and' => [
-//                ['foo' => 'bar', 'foobar' => 'foobar'],
-//                ['bar' => 'foo', 'barf' => 'barf'],
-//            ],
-//        ];
-//
-//        $efilter = '(foo= ? AND foobar= ?) AND (bar= ? AND barf= ?)';
-//        $evalues = ['bar', 'foobar', 'foo', 'barf'];
-//
-//        list($filter, $values) = $sqlSrvUsers->transformQuery($query);
-//        $this->assertSame($efilter, $filter);
-//        $this->assertSame($evalues, $values);
-//    }
+    public function testSetupDefaultSettings()
+    {
+        $wrapper = $this->createMock(SqlSrvWrapper::class);
+        $sqlSrvUsers = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $wrapper, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class));
+        $sqlSrvUsers->setup();
+    }
 
-//
-//    public function testTransformIsNull()
-//    {
-//        $pdo = new Pdo('foo', EndpointInterface::TYPE_DESTINATION, 'foobar', $this->createMock(PdoWrapper::class), $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class));
-//
-//        $query = [
-//            'foo' => null,
-//        ];
-//
-//        $efilter = '(foo IS NULL)';
-//        $evalues = [];
-//
-//        list($filter, $values) = $pdo->transformQuery($query);
-//        $this->assertSame($efilter, $filter);
-//        $this->assertSame($evalues, $values);
-//    }
-//
-//    public function testTransformNotNull()
-//    {
-//        $pdo = new Pdo('foo', EndpointInterface::TYPE_DESTINATION, 'foobar', $this->createMock(PdoWrapper::class), $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class));
-//
-//        $query = [
-//            'foo' => ['$ne' => null],
-//        ];
-//
-//        $efilter = '(foo IS NOT NULL)';
-//        $evalues = [];
-//
-//        list($filter, $values) = $pdo->transformQuery($query);
-//        $this->assertSame($efilter, $filter);
-//        $this->assertSame($evalues, $values);
-//    }
+    public function testShutdown()
+    {
+        $wrapper = $this->createMock(SqlSrvWrapper::class);
+        $sqlSrvUsers = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $wrapper, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class));
+        $sqlSrvUsers->shutdown();
+    }
+
+    public function testTransformAndQuery()
+    {
+        $sqlSrvUsers = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $this->createMock(Wrapper::class), $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class));
+
+        $query = [
+            '$and' => [
+                ['foo' => 'bar', 'foobar' => 'foobar'],
+                ['bar' => 'foo', 'barf' => 'barf'],
+            ],
+        ];
+
+        $efilter = '(foo= ? AND foobar= ?) AND (bar= ? AND barf= ?)';
+        $evalues = ['bar', 'foobar', 'foo', 'barf'];
+
+        list($filter, $values) = $sqlSrvUsers->transformQuery($query);
+        $this->assertSame($efilter, $filter);
+        $this->assertSame($evalues, $values);
+    }
+
+    public function testCountNoFilterReturnsTotal()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $mock->method('getQueryResult')->willReturn([
+            ['count' => 1],
+        ]);
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+        $result = $ep->count();
+        $this->assertEquals(1, $result);
+    }
+
+    public function testGetAllNoFilterReturnsTotal()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $mock->method('getQueryResult')->willReturn([
+            ['foo' => 'bar'],
+        ]);
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+
+        $this->assertSame(['foo' => 'bar'], iterator_to_array($ep->getAll())[0]->getData());
+        $this->assertCount(1, iterator_to_array($ep->getAll()));
+    }
+
+    public function testGetOne()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $mock->method('getQueryResult')->willReturn([
+            ['foo' => 'bar'],
+        ]);
+
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+
+        $result = $ep->getOne([])->getData();
+        $this->assertSame(['foo' => 'bar'], $result);
+    }
+
+    public function testCreateMechanismWindows()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $mock->expects($this->once())->method('query')->with('CREATE LOGIN [foobar] FROM WINDOWS');
+        $mock->method('getQueryResult')->willReturn([
+            ['principal_id' => 1],
+        ]);
+
+        $object = [
+            'mechanism' => 'windows',
+            'loginName' => 'foobar',
+        ];
+
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+
+        $result = $ep->create($this->createMock(AttributeMapInterface::class), $object);
+        $this->assertEquals(1, $result);
+    }
+
+    public function testCreateMechanismWindowsWithSql()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $mock->expects($this->at(1))->method('query')->with('CREATE LOGIN [foobar] FROM WINDOWS');
+        $mock->expects($this->at(2))->method('query')->with('CREATE USER [bar] FOR LOGIN [foobar]');
+        $mock->method('getQueryResult')->willReturn([
+            ['principal_id' => 1],
+        ]);
+
+        $object = [
+            'mechanism' => 'windows',
+            'loginName' => 'foobar',
+            'sqlName' => 'bar',
+        ];
+
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+
+        $result = $ep->create($this->createMock(AttributeMapInterface::class), $object);
+        $this->assertEquals(1, $result);
+    }
+
+    public function testCreateMechanismWindowsWithSqlAndRoles()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $mock->expects($this->at(1))->method('query')->with('CREATE LOGIN [foobar] FROM WINDOWS');
+        $mock->expects($this->at(2))->method('query')->with('CREATE USER [bar] FOR LOGIN [foobar]');
+        $mock->expects($this->at(3))->method('query')->with('EXEC sp_addrolemember foobarrole, [bar]');
+        $mock->method('getQueryResult')->willReturn([
+            ['principal_id' => 1],
+        ]);
+
+        $object = [
+            'mechanism' => 'windows',
+            'loginName' => 'foobar',
+            'sqlName' => 'bar',
+            'userRoles' => ['foobarrole'],
+        ];
+
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+
+        $result = $ep->create($this->createMock(AttributeMapInterface::class), $object);
+        $this->assertEquals(1, $result);
+    }
+
+    public function testCreateMechanismWindowWithSqlInvalidQuery()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $mock->expects($this->at(2))
+            ->method('query')
+            ->will($this->throwException(new InvalidQuery()));
+
+        $object = [
+            'mechanism' => 'windows',
+            'loginName' => 'foobar',
+            'sqlName' => 'bar',
+            'userRoles' => ['foobarrole'],
+        ];
+
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+
+        $result = $ep->create($this->createMock(AttributeMapInterface::class), $object);
+        $this->assertEquals(null, $result);
+    }
+
+    public function testCreateMechanismLocalMissingPassword()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $this->expectException(AttributeNotResolvable::class);
+        $mock->method('getQueryResult')->willReturn([
+            ['principal_id' => 1],
+        ]);
+
+        $object = [
+            'mechanism' => 'local',
+            'loginName' => 'foobar',
+        ];
+
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+
+        $result = $ep->create($this->createMock(AttributeMapInterface::class), $object);
+        $this->assertEquals(1, $result);
+    }
+
+    public function testCreateMechanismLocalInvalidQuery()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $mock->expects($this->once())
+            ->method('query')
+            ->will($this->throwException(new InvalidQuery()));
+
+        $object = [
+            'mechanism' => 'local',
+            'loginName' => 'foobar',
+            'password' => 'P@ssword',
+        ];
+
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+
+        $result = $ep->create($this->createMock(AttributeMapInterface::class), $object);
+        $this->assertEquals(null, $result);
+    }
+
+    public function testCreateMechanismLocalChangePwd()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $mock->expects($this->once())->method('query')->with('CREATE LOGIN [foobar] WITH PASSWORD = \'P@ssword\' MUST_CHANGE, CHECK_EXPIRATION = ON');
+        $mock->method('getQueryResult')->willReturn([
+            ['principal_id' => 1],
+        ]);
+
+        $object = [
+            'mechanism' => 'local',
+            'loginName' => 'foobar',
+            'password' => 'P@ssword',
+        ];
+
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+
+        $result = $ep->create($this->createMock(AttributeMapInterface::class), $object);
+        $this->assertEquals(1, $result);
+    }
+
+    public function testCreateMechanismLocal()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $mock->expects($this->once())->method('query')->with('CREATE LOGIN [foobar] WITH PASSWORD = \'P@ssword\'');
+        $mock->method('getQueryResult')->willReturn([
+            ['principal_id' => 1],
+        ]);
+
+        $object = [
+            'mechanism' => 'local',
+            'loginName' => 'foobar',
+            'password' => 'P@ssword',
+            'hasToChangePwd' => false,
+        ];
+
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+
+        $result = $ep->create($this->createMock(AttributeMapInterface::class), $object);
+        $this->assertEquals(1, $result);
+    }
+
+    public function testCreateMechanismLocalDisabled()
+    {
+        $mock = $this->createMock(Wrapper::class);
+        $mock->expects($this->at(1))->method('query')->with('CREATE LOGIN [foobar] WITH PASSWORD = \'P@ssword\'');
+        $mock->expects($this->at(2))->method('query')->with('ALTER LOGIN [foobar] DISABLE');
+        $mock->method('getQueryResult')->willReturn([
+            ['principal_id' => 1],
+        ]);
+
+        $object = [
+            'mechanism' => 'local',
+            'loginName' => 'foobar',
+            'password' => 'P@ssword',
+            'hasToChangePwd' => false,
+            'disabled' => true,
+        ];
+
+        $ep = new SqlSrvUsers('foo', EndpointInterface::TYPE_DESTINATION, $mock, $this->createMock(CollectionInterface::class), $this->createMock(WorkflowFactory::class), $this->createMock(LoggerInterface::class), [
+            'data' => ['options' => ['filter_one' => '{"uid":"foo"}']],
+        ]);
+
+        $result = $ep->create($this->createMock(AttributeMapInterface::class), $object);
+        $this->assertEquals(1, $result);
+    }
 }
