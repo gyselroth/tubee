41a53e5a91f07007ffb79f2a7187b7f1f3400995
s-aebischer <aebischer@gyselroth.com>
2022-08-18 14:52:22 +0200

    #95 updated copyright year

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,7 +3,7 @@
 $header = <<<'EOF'
 tubee
 
-@copyright   Copryright (c) 2017-2021 gyselroth GmbH (https://gyselroth.com)
+@copyright   Copryright (c) 2017-2022 gyselroth GmbH (https://gyselroth.com)
 @license     GPL-3.0 https://opensource.org/licenses/GPL-3.0
 EOF;
 $config = (new PhpCsFixer\Config());
