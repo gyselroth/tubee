d6003d905b405dd037bf90d6362a38e3e80ccad8
s-aebischer <aebischer@gyselroth.com>
2025-04-23 23:02:44 +0200

    MattermostEndpoint

---
+++ src/lib/Endpoint/Mattermost/Factory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * tubee
+ *
+ * @copyright   Copryright (c) 2017-2022 gyselroth GmbH (https://gyselroth.com)
+ * @license     GPL-3.0 https://opensource.org/licenses/GPL-3.0
+ */
+
+namespace Tubee\Endpoint\Mattermost;
+
+use Psr\Log\LoggerInterface;
+use Tubee\Collection\CollectionInterface;
+use Tubee\Endpoint\Mattermost;
+use Tubee\Endpoint\EndpointInterface;
+use Tubee\Endpoint\Rest\Factory as RestFactory;
+use Tubee\Workflow\Factory as WorkflowFactory;
+
+class Factory
+{
+    /**
+     * Build instance.
+     */
+    public static function build(array $resource, CollectionInterface $collection, WorkflowFactory $workflow_factory, LoggerInterface $logger): EndpointInterface
+    {
+        $client = RestFactory::buildClient($resource, $logger);
+
+        return new Mattermost($resource['name'], $resource['data']['type'], $client, $collection, $workflow_factory, $logger, $resource);
+    }
+}
